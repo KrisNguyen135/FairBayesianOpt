75394a7f8678e71b242be85a1f2dd2261816924d
KrisNguyen135 <nguyenminhquan135@gmail.com>
2020-02-07 14:38:15 -0600

    added utility vs fairness opt

--- quan_utils.py
+++ quan_utils.py
@@ -10,14 +10,13 @@ import time
 
 class AssignmentHelper:
     def __init__(self):
-        self.prob_df = pd.read_csv('../../data/data.csv', index_col=0)  #.iloc[: 1000, :]
+        self.prob_df = pd.read_csv('../../data/data.csv', index_col=0)  #.iloc[: 2000, :]
 
         self.households = [i for i in range(1, self.prob_df.shape[0] + 1)]
         self.types = ['ES', 'PSH', 'TH', 'RRH', 'PREV']
 
     def update_constraints(self, fairness_constraint=None, capacity_df=None):
         # Variable containing individual assignments
-        print('Creating variables')
         self.x = pulp.LpVariable.dicts(
             'assignment',
             [(household, type_) for household in self.households
@@ -26,33 +25,28 @@ class AssignmentHelper:
         )
 
         # IP problem
-        print('Setting up problem')
         self.prob = pulp.LpProblem('homelessness', pulp.LpMinimize)
 
         # Objective function
-        print('Setting up objective function')
-        self.prob += sum(self.x[(household, type_)] * self.prob_df.loc[household, type_]
-                         for household in self.households for type_ in self.types)
+        self.prob += pulp.lpSum(self.x[(household, type_)] * self.prob_df.loc[household, type_]
+                                for household in self.households for type_ in self.types)
 
         # Assignment constraint
-        print('Creating assignment constraint')
         for household in self.households:
-            self.prob += sum(self.x[(household, type_)] for type_ in self.types) == 1
+            self.prob += pulp.lpSum(self.x[(household, type_)] for type_ in self.types) == 1
 
         # Capacity constraint
-        print('Creating capacity constraint')
         if capacity_df is None:
             capacity_df = self.prob_df['Real'].value_counts()  # original constraint
         for type_index, type_ in enumerate(self.types):
-            self.prob += sum(
+            self.prob += pulp.lpSum(
                 self.x[(household, type_)] for household in self.households
             ) <= capacity_df.loc[type_index + 1]
 
         # Fairness constraint
         if fairness_constraint is not None:
-            print('Creating fairness constraint')
             for household in self.households:
-                self.prob += sum(
+                self.prob += pulp.lpSum(
                     self.x[(household, type_)] * self.prob_df.loc[household, type_]
                     for type_ in self.types
                 ) <= self.prob_df.loc[
@@ -103,7 +97,7 @@ class AssignmentHelper:
 
         return self.prob_df['Reentered'].sum(), bart_running_result, running_result
 
-    def visualize_increased_prob(self, assignment_df, name=None, include_zero=False):
+    def compare_prob(self, assignment_df, name=None):
         if name is None:
             name = 'Input assignment'
         prob_compare_df = pd.DataFrame(columns=['Real', name])
@@ -121,8 +115,9 @@ class AssignmentHelper:
 
         # Difference between assigned and real probabilities
         prob_compare_df['Diff'] = prob_compare_df[name] - prob_compare_df['Real']
-        if include_zero:
-            plt.hist(prob_compare_df['Diff'], bins=30)
-        else:
-            plt.hist(prob_compare_df[prob_compare_df['Diff'] != 0]['Diff'], bins=30)
-        plt.show()
+        # if include_zero:
+        #     plt.hist(prob_compare_df['Diff'], bins=30)
+        # else:
+        #     plt.hist(prob_compare_df[prob_compare_df['Diff'] != 0]['Diff'], bins=30)
+        # plt.show()
+        return prob_compare_df
